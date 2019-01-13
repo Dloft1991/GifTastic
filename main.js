c05d32e503861badb122b2821de441e29677a062
Dloft <derek.lofton1@gmail.com>
2019-01-13 14:21:49 -0500

    wrongdataattr

--- main.js
+++ main.js
@@ -1,8 +1,43 @@
 
+// array for buttons
+var topics = ["Iron Man", "Thor", "Captain America", "Hulk"]
+
+// make movie buttons
+function topicButton() {
+    $("#topic-button").empty();
+
+    //for loop the array
+    for (var i = 0; i < topics.length; i++) {
+    
+        var T = $("<button>");
+
+        T.addClass("topic-btn");
+        T.text(topics[i]);
+
+        $("#topic-button").append(T);
+    }
+}
+
+function buttonMaker() {
+    $("#topic-button").empty();
+
+    //for loop through the array and make a butotn
+    for (var i = 0; i < topics.length; i++) {
+        var b = $("<button>");
+
+        b.addClass("topic-btn");
+        b.attr("data-name", topics[i]);
+        b.text(topics[i]);
+
+        $("#topic-button").append(b);
+    }
+}
+buttonMaker();
+
   // on click of putton
-  $("button").on("click", function() {
+  $("#topic-button").on("click", function() {
       // this refers to the button that was clicked
-    var person = $(this).attr("data-person");
+    var person = $(this).attr("data-name");
 
    // url for API
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
@@ -12,29 +47,30 @@
       url: queryURL,
       method: "GET"
     })
-      
-      .then(function(response) {
+    .then(function(response) {
+        console.log(response);
+        $("#getGif").text(response);
+
         var results = response.data;
-    
+
         for (var i = 0; i < results.length; i++) {
 
-        
-          if (results[i].rating !== "r" && results[i].rating !== "PG-13") {
+            var topicDiv = $("div");
+            //for the rating
+            var p = $("<p>").text("Rating: " + results[i].rating);
+            // for the image
+            var topicImage = $("<img>");
 
-            var gifDiv = $("<div>");
-            var rating = results[i].rating;
+            topicImage.attr("src", results[i].images.fixed_height.url);
 
-    // where the rating goes
-            var p = $("<p>").text("Rating: " + rating);
-// where the image goes
-            var personImage = $("<img>");
-            personImage.attr("src", results[i].images.fixed_height.url);
-
-            gifDiv.append(p);
-            gifDiv.append(personImage);
-
-            $("#gifs-appear-here").prepend(gifDiv);
-          }
+            topicDiv.append(p);
+            topicDiv.append(topicImage);
+            // 
+            $("#get-gif").prepend(topicDiv);
         }
-      });
-  });
+    });
+    
+});
+
+      
+    
