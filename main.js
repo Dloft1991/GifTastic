7935d44ffe71ba4aeda09c9ec6dd551c0e9b3319
Dloft <derek.lofton1@gmail.com>
2019-01-16 08:02:52 -0500

    submit

--- main.js
+++ main.js
@@ -31,48 +31,76 @@ $("button").on("click", function () {
 
                 $("#get-gif").prepend(heroDiv);
             }
+
         });
 });
 
 function makeButton() {
 
-    // Deleting the movies prior to adding new movies
-    // (this is necessary otherwise you will have repeat buttons)
+    
     $("#buttons-view").empty();
 
     // Looping through the array of movies
     for (var i = 0; i < heros.length; i++) {
 
-      // Then dynamicaly generating buttons for each movie in the array
-      // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-      var a = $("<button>");
-      // Adding a class of movie-btn to our button
-      a.addClass("movie-btn");
-      // Adding a data-attribute
-      a.attr("data-name", heros[i]);
-      // Providing the initial button text
-      a.text(heros[i]);
-      // Adding the button to the buttons-view div
-      $("#buttons-view").append(a);
+    
+        var a = $("<button>");
+        a.addClass("movie-btn");
+        a.attr("data-name", heros[i]);
+        
+        a.text(heros[i]);
+        $("#buttons-view").append(a);
     }
     console.log(heros);
-  }
+}
 
-  // This function handles events where a movie button is clicked
-  $("#add-hero").on("click", function(event) {
+
+
+//add hero button
+$("#add-hero").on("click", function (event) {
     event.preventDefault();
-    // This line grabs the input from the textbox
+    
     var hero = $("#hero-input").val().trim();
-
-    // Adding movie from the textbox to our array
     heros.push(hero);
 
-    // Calling renderButtons which handles the processing of our movie array
+    //makes button from array
     makeButton();
-  });
+    
+});
 
-  // Adding a click event listener to all elements with a class of "movie-btn"
+
+// Adding a click event listener to all elements with a class of "movie-btn"
 //   $(document).on("click", ".movie-btn", displayMovieInfo);
 
-  // Calling the renderButtons function to display the intial buttons
-  makeButton();
+// Calling the renderButtons function to display the intial buttons
+makeButton();
+
+
+///////////////////000000000000
+function displayMovieInfo() {
+
+    var movie = $(this).attr("data-name");
+    var queryURL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+      
+        for (var i = 0; i < results.length; i++) {
+
+            var heroDiv = $("<div>");
+            var p = $("<p>").text("Rating: " + results[i].rating);
+            var heroImage = $("<img>");
+
+            heroImage.attr("src", results[i].images.fixed_height.url);
+            heroDiv.append(p);
+            heroDiv.append(heroImage);
+
+            $("#get-gif").prepend(heroDiv);
+        }
+
+      });
+
+
+}
